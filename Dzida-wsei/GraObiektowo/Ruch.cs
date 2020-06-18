5386f27fbb9394304589bbbba6b59bdd98757f0d
Dzida-wsei <62931529+Dzida-wsei@users.noreply.github.com>
2020-06-18 19:28:43 +0200

    Create Ruch.cs

---
+++ Dzida-wsei/GraObiektowo/Ruch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraObiektowo
+{
+    public class Ruch
+    {
+        public DateTime Czas { get; }
+        public int Propozycja { get; }
+        public ModelGry.Odpowiedz Wynik { get; }
+        public ModelGry.Status StatusGry { get; }
+
+        public Ruch(int propozycja, ModelGry.Odpowiedz wynik, ModelGry.Status statusGry)
+        {
+            Propozycja = propozycja;
+            Wynik = wynik;
+            StatusGry = statusGry;
+            Czas = DateTime.Now;
+        }
+
+        public override string ToString() => $"({Propozycja}, {Wynik}, {Czas}, {StatusGry})";
+    }
+}
